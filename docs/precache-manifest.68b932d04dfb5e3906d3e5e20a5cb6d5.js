20876a3a78eed692fa3d7617edd53d9230b1ac68
Kacper Koza <kacperkoza34@gmail.com>
2020-05-30 15:19:26 +0200

    Build

---
+++ docs/precache-manifest.68b932d04dfb5e3906d3e5e20a5cb6d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fbc8d9142d3c4346aa8d3c973786e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebbb81fe3c1dadd62768",
+    "url": "/static/css/main.71d486b4.chunk.css"
+  },
+  {
+    "revision": "f64237641a004a527e46",
+    "url": "/static/js/2.641a1016.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.641a1016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbb81fe3c1dadd62768",
+    "url": "/static/js/main.e1bade70.chunk.js"
+  },
+  {
+    "revision": "f27cec6117f79885bbc3",
+    "url": "/static/js/runtime-main.02419f45.js"
+  }
+]);
